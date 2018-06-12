7351b3712bed9df405489ffdb5101b9b9f5477c2
orlyohreally <orli-knop@ya.ru>
2018-06-12 12:06:14 +0300

    configured menu navigation toggle

--- client/src/app/main-menu/main-menu.component.ts
+++ client/src/app/main-menu/main-menu.component.ts
@@ -2,24 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../modal.service';
 import { LoginFormComponent } from '../login-form/login-form.component';
 
-//import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { AppComponent } from '../app.component';
 
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.css']
+    selector: 'app-main-menu',
+    templateUrl: './main-menu.component.html',
+    styleUrls: ['./main-menu.component.css'],
+    host: {
+        '(window:resize)': 'onResize($event)'
+    }
 })
 export class MainMenuComponent implements OnInit {
+    collapsed: boolean;
+    constructor(private modalService: ModalService, private appComponent: AppComponent) {}
 
-  constructor(private modalService: ModalService, private appComponent: AppComponent) {}
-
-  ngOnInit() { }
-  ShowLoginForm() {
-    this.appComponent.LoadComponentModal(LoginFormComponent);
-  }
-  CloseLoginForm() {
-    this.appComponent.CloseComponentModal();
-  }
+    ngOnInit() {
+        this.collapsed = false;
+    }
+    onResize(event) {
+        console.log(event, event.target.innerWidth);
+        if(event.target.innerWidth > 768)
+            this.collapsed = false;
+    }
+    toggleMobileMenu() {
+        this.collapsed = !this.collapsed;
+    }
+    ShowLoginForm() {
+        this.collapsed = false;
+        this.appComponent.LoadComponentModal(LoginFormComponent);
+    }
+    
+    CloseLoginForm() {
+        this.appComponent.CloseComponentModal();
+    }
 }
